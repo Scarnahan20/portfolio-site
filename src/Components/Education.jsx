f77c568df6470eb25b718a56dff83b388c67c133
Scarnahan20 <55592707+Scarnahan20@users.noreply.github.com>
2020-11-24 08:07:43 -0800

    Beginning to add images to edu cards

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -7,20 +7,28 @@ function Education() {
             <h1>Education</h1>
             <CardDeck>
                 <Card>
-                    <Card.Body>
+                    <Card.Img className="education-img" src="./sdsu.jpg" />
+                    <Card.ImgOverlay>
                         <Card.Title>San Diego State University</Card.Title>
+                        <Card.Subtitle className="mb-2">
+                            Bachelor of Science
+                            <br />
+                            Computer Science
+                        </Card.Subtitle>
                         <Card.Text>Graduated May 2020</Card.Text>
-                    </Card.Body>
+                    </Card.ImgOverlay>
                 </Card>
-                <Card className="education-cards">
+                <Card>
                     <Card.Body>
                         <Card.Title>The Complete 2020 Web Development Bootcamp</Card.Title>
+                        <Card.Subtitle className="mb-2">Udemy.com Online Learning</Card.Subtitle>
                         <Card.Text>Certificate Earned July 2020</Card.Text>
                     </Card.Body>
                 </Card>
-                <Card className="education-cards">
+                <Card>
                     <Card.Body>
                         <Card.Title>The Complete 2020 Flutter Development Bootcamp with Dart</Card.Title>
+                        <Card.Subtitle className="mb-2">Udemy.com Online Learning</Card.Subtitle>
                         <Card.Text>Certificate Earned November 2020</Card.Text>
                     </Card.Body>
                 </Card>
